1d1157dbef2b67b65b6922c225f436c6771fa208
kenny1224 <kenny1224@gmail.com>
2017-01-07 01:44:28 +0800

    1.增加簡單註解，調整排版 2.修改try-catch，將全部迴圈執行內容放入tryCatch 3.因存檔後excel檔案產生不明錯誤，將資料改為存入csv 4.增加存檔指定資料夾路徑 5.增加每500個小分類存檔及完成全部任務的顯示文字

--- pchome_parser_complete.R
+++ pchome_parser_complete.R
@@ -4,22 +4,18 @@ rm(list = ls())
 if (!require("pacman")) {install.packages("pacman")}
 pacman::p_load(httr, rvest, magrittr, jsonlite, dplyr, tidyr, lubridate, openxlsx)
 
-#設定分類對應網址參數
+#設定大分類對應網址代碼
 cate <- c("3c", "nb", "mobile", "digi", "ce", "cp")
 cate.name <- c("3C", "NB", "通訊", "數位", "家電", "周邊")
-# cate <- c("3c", "nb", "mobile", "digi", "ce") #cp(周邊)爬蟲過程有錯，先跳過
-# cate.name <- c("3C", "NB", "通訊", "數位", "家電") #cp(周邊)爬蟲過程有錯，先跳過
-# cate <- c("3c") #cp(周邊)爬蟲過程有錯，先跳過
-# cate.name <- c("3C") #cp(周邊)爬蟲過程有錯，先跳過
 
-#組合網址list
+#抓取中分類資料的網址list
 url.head <- "http://ecapi.pchome.com.tw/cdn/ecshop/cateapi/v1.5/region&sign=h24%252F"
 url.middle <- "&_callback=cb_ecshopCategoryRegion&"
-url.num <- 24724000 + as.integer(runif(1, 2000, 3000))
+url.num <- 24724000 + as.integer(runif(1, 2000, 3000)) #規則未知，隨機給一組數字
 
 cate.url.list <- paste0(url.head, cate, url.middle, url.num)
 
-#抓取子分類參數代碼
+#抓取中分類代碼資料
 get.sub.cate <- function(url){
     res_df <- url %>% 
         read_html() %>% html_text() %>% 
@@ -28,20 +24,24 @@ get.sub.cate <- function(url){
         fromJSON() 
     return(res_df)
 }
+
 sub.cate <- lapply(cate.url.list, get.sub.cate) %>% 'names<-'(cate.name)
 
+#大中分類名稱及對應代碼資料 sub.cate.df
 sub.cate.df <- data.frame()
 for(i in 1:length(cate.name)){
     temp <- sub.cate[[i]] %>% mutate(Category = names(sub.cate[i])) 
     sub.cate.df <- rbind(sub.cate.df, temp)
 }
 
+#抓取小分類資料的網址list
 sub.cate.url.head <- "http://ecapi.pchome.com.tw/cdn/ecshop/cateapi/v1.5/region/"
 cate.url.middle <- "/menu&_callback=jsonp_nemu&"
 subcate.url.end <- "?_callback=jsonp_nemu"
 
 cate.url.list <- paste0(sub.cate.url.head, sub.cate.df$Id, cate.url.middle, url.num, subcate.url.end)
 
+#抓取小分類代碼資料
 get.sub.sub.cate <- function(url){
     res_df <- url %>% 
         read_html() %>% html_text() %>% 
@@ -53,34 +53,38 @@ get.sub.sub.cate <- function(url){
 
 sub.sub.cate <- lapply(cate.url.list, get.sub.sub.cate) %>% 'names<-'(sub.cate.df$Id)
 
+#中小分類代碼及小分類名稱資料 sub.sub.cate.df
 sub.sub.cate.df <- data.frame()
 for(i in 1:length(sub.sub.cate)){
     temp <- sub.sub.cate[[i]] %>% mutate(Sub.Id = Id, Id = names(sub.sub.cate[i])) 
     sub.sub.cate.df <- rbind(sub.sub.cate.df, temp)
 }
 
+#大中小分類名稱及對應代碼資料 full.cate.df
 full.cate.df <- sub.cate.df %>% left_join(., sub.sub.cate.df, by = "Id")
 
+#抓取小分類代碼對應商品資料的網址list
 small.cate.url.head <- "http://ecapi.pchome.com.tw/ecshop/prodapi/v2/store/"
 small.cate.url.end <- "/prod&offset=8&limit=36&fields=Id,Nick,Pic,Price,Discount,isSpec,Name,isCarrier,isSnapUp,isBigCart&_callback=jsonp_prodgrid?_callback=jsonp_prodgrid"
 
 small.cate.url.list <- paste0(small.cate.url.head, full.cate.df$Sub.Id, small.cate.url.end)
 
 full.cate.df <- full.cate.df %>% 
-    mutate(small.cate.url.list = paste0(small.cate.url.head, full.cate.df$Sub.Id, small.cate.url.end))
+                mutate(small.cate.url.list = paste0(small.cate.url.head, full.cate.df$Sub.Id, small.cate.url.end))
 
+#抓取商品資料
 parse.small.cate <- function(url){
     res_df.temp <- url %>% 
-        read_html() %>% html_text() %>% 
-        gsub("try\\{jsonp_prodgrid\\(", "", .) %>% 
-        gsub("\\);\\}catch\\(e\\)\\{if\\(window.console\\)\\{console\\.log\\(e\\);\\}\\}", "", .) %>% 
-        fromJSON()
+                read_html() %>% html_text() %>% 
+                gsub("try\\{jsonp_prodgrid\\(", "", .) %>% 
+                gsub("\\);\\}catch\\(e\\)\\{if\\(window.console\\)\\{console\\.log\\(e\\);\\}\\}", "", .) %>% 
+                fromJSON()
     
     if(length(res_df.temp) == 0){
         res_df <- data.frame()
     }else{
         res_df <- res_df.temp %>% select(-c(Pic, Price)) %>% 
-            mutate(Price_M = res_df.temp$Price$M, Price_P = res_df.temp$Price$P)
+                mutate(Price_M = res_df.temp$Price$M, Price_P = res_df.temp$Price$P)
     }
     return(res_df)
 }
@@ -89,38 +93,50 @@ df.colnames <- c("Web.Id","Web.Sub.Id","Category","Name.1","Name.2",
                  "Prod.Id","Nick","Discount" ,"isSpec","Name",
                  "isCarrier","isSnapUp","isBigCart","Price_M","Price_P")
 
-pchome.cate.df <- data.frame()
+pchome.temp.df <- data.frame()
 ptm.start <- proc.time()
+
 for(n in 1:length(full.cate.df$small.cate.url.list)){
     tryCatch(
-        {small.cat.df <- parse.small.cate((full.cate.df$small.cate.url.list[n]))
+        {
+        small.cat.df <- parse.small.cate((full.cate.df$small.cate.url.list[n]))
+           
+        Sys.sleep(runif(1, 1, 3))
+        
+        ptm.end <- proc.time()
+        ptm.loop <- ptm.end - ptm.start
+        
+        cat(n, "/", length(small.cate.url.list), "sub-categories,", 
+            "progress:", sprintf("%1.2f%%", 100 * n/length(small.cate.url.list)),
+            ", spend", ptm.loop["elapsed"], "seconds", "\n")
+            
+            info.df <- full.cate.df[n, ] %>% as_data_frame() %>% 
+                       select(Id, Sub.Id, Category, Name.x, Name.y) %>%
+                       .[rep(seq_len(nrow(.)), each = nrow(small.cat.df)),]
+            
+            pchome.temp.df <- rbind(pchome.temp.df, cbind(info.df, small.cat.df))
+            
+                if(n %% 500 == 0){
+                    pchome.df <- pchome.temp.df %>% 'names<-'(df.colnames)
+                    pchome.df <- pchome.df %>% mutate(Parse.Date = as_date(today())) %>%
+                                select(Parse.Date, everything())
+                    write.csv(pchome.df, file = paste0("E:/Git/crawler/Data/pchome_temp.csv"))
+                    cat("Save", n, "small-categories records in temp csv file \n")
+                }
+            pchome.temp.df
+            
         },error = function(e){cat("ERROE:", conditionMessage(e), "\n")
         },warning = function(w){cat("WARNING:", warning(w), "\n")}
-    )
-    Sys.sleep(runif(1, 3, 5))
-    ptm.end <- proc.time()
-    ptm.loop <- ptm.end - ptm.start
-    cat(n, "/", length(small.cate.url.list), "sub-categories,", 
-        "progress:", sprintf("%1.2f%%", 100 * n/length(small.cate.url.list)),
-        ", spend", ptm.loop["elapsed"], "seconds", "\n")
-    
-    info.df <- full.cate.df[n, ] %>% as_data_frame() %>% 
-        select(Id, Sub.Id, Category, Name.x, Name.y) %>%
-        .[rep(seq_len(nrow(.)), each = nrow(small.cat.df)),]
-    pchome.cate.df <- rbind(pchome.cate.df, cbind(info.df, small.cat.df))
-    if(n %% 200 == 0){
-        pchome.df <- pchome.cate.df %>% 'names<-'(df.colnames)
-        pchome.df <- pchome.df %>% mutate(Parse.Date = as_date(today())) %>%
-            select(Parse.Date, everything())
-        write.xlsx(pchome.df, file = "pchome_temp.xlsx", sheetName = paste(today()))
-    }
-    pchome.cate.df
+        )
 }
 
-colnames(pchome.cate.df) <- df.colnames
+colnames(pchome.temp.df) <- df.colnames
 
-pchome.df <- pchome.cate.df %>% mutate(Parse.Date = as_date(today())) %>%
+pchome.df <- pchome.temp.df %>% mutate(Parse.Date = as_date(today())) %>%
     select(Parse.Date, everything())
 
-saveRDS(pchome.df, "pchome_df.rds")
-write.xlsx(pchome.df, file = paste0("pchome_", today(), ".xlsx"), sheetName = paste(today()))
+saveRDS(pchome.df, paste0("E:/Git/crawler/Data/pchome_", gsub(x = today(), "-", ""), ".rds"))
+write.csv(pchome.df, file = paste0("E:/Git/crawler/Data/pchome_", gsub(x = today(), "-", ""), ".csv"))
+
+cat("Mission complete,", nrow(pchome.temp.df), paste0("records save in \"pchome_", gsub(x = today(), "-", ""), ".csv\""))
+
